b6cd114bc8b3b72accda370aaa0ddf2b6e8bbc79
Cayo B. Gomes <ultrajedimaster@gmail.com>
2026-04-09 12:36:52 -0300

    Testando a branch

--- Trilhas/Controllers/AccountController.cs
+++ Trilhas/Controllers/AccountController.cs
@@ -10,7 +10,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Trilhas.Controllers
-{
+{//Teste
     public class AccountController : Controller
     {
         private readonly IWebHostEnvironment _env;
